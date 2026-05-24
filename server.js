a50c7a8afa3c97894592cbf43038ee02a9d3eec4
Ashwinmathew452 <ashwinmathew206@gmail.com>
2026-05-24 11:58:51 +0530

    added security improvements and styled UI

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests. Please try again later."
+});
+
+app.use(limiter);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const indexRoutes = require("./routes/index");
+const routes = require("./routes/index");
 
-app.use("/", indexRoutes);
+app.use("/", routes);
 
 app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
